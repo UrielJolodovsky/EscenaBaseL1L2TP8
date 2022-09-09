97e0bad3104328a063e153de469c5f19dee8f091
UrielJolodovsky <101568983+UrielJolodovsky@users.noreply.github.com>
2022-09-09 08:05:58 -0300

    Pick Up a component continued

--- Assets/Scripts/PickUPObject.cs
+++ Assets/Scripts/PickUPObject.cs
@@ -7,6 +7,7 @@ public class PickUPObject : MonoBehaviour
     public GameObject elementoAgarrado;
     public bool Agarrado = false;
     public GameObject Jugador;
+    [SerializeField] bool[] ComponentesComputadora;
     // Start is called before the first frame update
     void Start()
     {
@@ -26,7 +27,7 @@ public class PickUPObject : MonoBehaviour
     }
     void OnTriggerStay(Collider other)
     {
-        if (Input.GetKeyDown(KeyCode.E) && Agarrado == false && other.gameObject.tag != "NPC")
+        if (Input.GetKeyDown(KeyCode.E) && Agarrado == false && other.gameObject.tag != "NPC" && other.gameObject.tag != "mesa")
         {
             elementoAgarrado = other.gameObject;
             elementoAgarrado.SetActive(false);
@@ -36,47 +37,57 @@ public class PickUPObject : MonoBehaviour
         {
             if (Input.GetKeyDown(KeyCode.T) && other.gameObject.layer == 7)
             {
-                if (elementoAgarrado.gameObject.tag == "Monitor")
+                if (elementoAgarrado.gameObject.tag == "Monitor" && ComponentesComputadora[0] == false)
                 {
-
+                    ComponentesComputadora[0] = true;
+                    elementoAgarrado.transform.position = new Vector3 (40,40,40);
+                    elementoAgarrado = null;
                 }
-                else if (elementoAgarrado.gameObject.tag == "Mouse")
+                else if (elementoAgarrado.gameObject.tag == "Mouse" && ComponentesComputadora[1] == false)
                 {
-
+                    ComponentesComputadora[1] = true;
+                    elementoAgarrado = null;
                 }
-                else if (elementoAgarrado.gameObject.tag == "Teclado")
+                else if (elementoAgarrado.gameObject.tag == "Teclado" && ComponentesComputadora[2] == false)
                 {
-
+                    ComponentesComputadora[2] = true;
+                    elementoAgarrado = null;
                 }
             }
             else if (Input.GetKeyDown(KeyCode.T) && other.gameObject.layer == 8)
             {
-                if (elementoAgarrado.gameObject.tag == "Monitor")
+                if (elementoAgarrado.gameObject.tag == "Monitor" && ComponentesComputadora[3] == false)
                 {
-
+                    ComponentesComputadora[3] = true;
+                    elementoAgarrado = null;
                 }
-                else if (elementoAgarrado.gameObject.tag == "Mouse")
+                else if (elementoAgarrado.gameObject.tag == "Mouse" && ComponentesComputadora[4] == false)
                 {
-
+                    ComponentesComputadora[4] = true;
+                    elementoAgarrado = null;
                 }
-                else if (elementoAgarrado.gameObject.tag == "Teclado")
+                else if (elementoAgarrado.gameObject.tag == "Teclado" && ComponentesComputadora[5] == false)
                 {
-
+                    ComponentesComputadora[5] = true;
+                    elementoAgarrado = null;
                 }
             }
             else if (Input.GetKeyDown(KeyCode.T) && other.gameObject.layer == 9)
             {
-                if (elementoAgarrado.gameObject.tag == "Monitor")
+                if (elementoAgarrado.gameObject.tag == "Monitor" && ComponentesComputadora[6] == false)
                 {
-
+                    ComponentesComputadora[6] = true;
+                    elementoAgarrado = null;
                 }
-                else if (elementoAgarrado.gameObject.tag == "Mouse")
+                else if (elementoAgarrado.gameObject.tag == "Mouse" && ComponentesComputadora[7] == false)
                 {
-
+                    ComponentesComputadora[7] = true;
+                    elementoAgarrado = null;
                 }
-                else if (elementoAgarrado.gameObject.tag == "Teclado")
+                else if (elementoAgarrado.gameObject.tag == "Teclado" && ComponentesComputadora[8] == false)
                 {
-
+                    ComponentesComputadora[8] = true;
+                    elementoAgarrado = null;
                 }
             }
         }
