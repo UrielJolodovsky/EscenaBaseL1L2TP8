3ef445992c81df61266e5d908ac08a409ec24dec
UrielJolodovsky <101568983+UrielJolodovsky@users.noreply.github.com>
2022-09-09 10:14:03 -0300

    Pick up an object continued

--- Assets/Scripts/PickUPObject.cs
+++ Assets/Scripts/PickUPObject.cs
@@ -35,58 +35,66 @@ public class PickUPObject : MonoBehaviour
         }
         if (other.gameObject.tag == "mesa" && Agarrado == true)
         {
-            if (Input.GetKeyDown(KeyCode.T) && other.gameObject.layer == 7)
+            if (Input.GetKeyDown(KeyCode.T) && other.gameObject.layer == 9)
             {
                 if (elementoAgarrado.gameObject.tag == "Monitor" && ComponentesComputadora[0] == false)
                 {
                     ComponentesComputadora[0] = true;
-                    elementoAgarrado.transform.position = new Vector3 (40,40,40);
+                    elementoAgarrado.transform.position = new Vector3 (-2.828f, -0.09f, -6.8f);
                     elementoAgarrado = null;
                 }
                 else if (elementoAgarrado.gameObject.tag == "Mouse" && ComponentesComputadora[1] == false)
                 {
                     ComponentesComputadora[1] = true;
+                    elementoAgarrado.transform.position = new Vector3(-2.457f, -0.29f, -6.49f);
                     elementoAgarrado = null;
                 }
                 else if (elementoAgarrado.gameObject.tag == "Teclado" && ComponentesComputadora[2] == false)
                 {
                     ComponentesComputadora[2] = true;
+                    elementoAgarrado.transform.position = new Vector3(-2.484f, -0.3f, -6.776f);
                     elementoAgarrado = null;
                 }
             }
-            else if (Input.GetKeyDown(KeyCode.T) && other.gameObject.layer == 8)
+            else if (Input.GetKeyDown(KeyCode.T) && other.gameObject.layer == 10)
             {
                 if (elementoAgarrado.gameObject.tag == "Monitor" && ComponentesComputadora[3] == false)
                 {
                     ComponentesComputadora[3] = true;
+                    elementoAgarrado.transform.position = new Vector3(-2.828f, -0.09f, -5.075f);
                     elementoAgarrado = null;
                 }
                 else if (elementoAgarrado.gameObject.tag == "Mouse" && ComponentesComputadora[4] == false)
                 {
                     ComponentesComputadora[4] = true;
+                    elementoAgarrado.transform.position = new Vector3(-2.457f, -0.29f, -4.759f);
                     elementoAgarrado = null;
                 }
                 else if (elementoAgarrado.gameObject.tag == "Teclado" && ComponentesComputadora[5] == false)
                 {
                     ComponentesComputadora[5] = true;
+                    elementoAgarrado.transform.position = new Vector3(-2.484f, -0.3f, -5.111f);
                     elementoAgarrado = null;
                 }
             }
-            else if (Input.GetKeyDown(KeyCode.T) && other.gameObject.layer == 9)
+            else if (Input.GetKeyDown(KeyCode.T) && other.gameObject.layer == 11)
             {
                 if (elementoAgarrado.gameObject.tag == "Monitor" && ComponentesComputadora[6] == false)
                 {
                     ComponentesComputadora[6] = true;
+                    elementoAgarrado.transform.position = new Vector3(-2.828f, -0.09f, -3.358f);
                     elementoAgarrado = null;
                 }
                 else if (elementoAgarrado.gameObject.tag == "Mouse" && ComponentesComputadora[7] == false)
                 {
                     ComponentesComputadora[7] = true;
+                    elementoAgarrado.transform.position = new Vector3(-2.457f, -0.29f, -3.156f);
                     elementoAgarrado = null;
                 }
                 else if (elementoAgarrado.gameObject.tag == "Teclado" && ComponentesComputadora[8] == false)
                 {
                     ComponentesComputadora[8] = true;
+                    elementoAgarrado.transform.position = new Vector3(-2.484f, -0.3f, -3.492f);
                     elementoAgarrado = null;
                 }
             }
